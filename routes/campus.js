4f59a48353e9ededf221edfa15bd61b064d7b071
Muertoparado <nicolas.casanova20@gmail.com>
2023-08-08 19:58:17 -0500

    Added validator to collection "menos" and added an insert route to the routes/campus.js file.

--- routes/campus.js
+++ routes/campus.js
@@ -10,4 +10,25 @@ appCampus.get("/", limitGrt(),async(req, res)=>{
     console.log(req.rateLimit);
 });
 
+appCampus.post('/insert', function (req, res) {
+    ObjectId.connect(con(), function(err, db) {
+      if (err) {
+        console.log("Error de conexión:", err);
+        res.status(500).send("Error de conexión a la base de datos");
+        return;
+      }
+      var dbo = db.db("practica1");
+      var myobj = req.body;
+      dbo.collection("usuario").insertOne(myobj)
+      .then(result => {
+        console.log("Documento insertado");
+        res.send({ status: "success", message: "Documento insertado", document: result.ops[0] });
+        db.close();
+      })
+      .catch(error => {
+        console.log("Error al insertar documento:", error);
+        res.status(500).send("Error al insertar documento");
+      });
+    });
+  });
 export default appCampus;
